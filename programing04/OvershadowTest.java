1490668bbab143653cf1a8f6e7fee5f991c06a9a
elio037 <casey030703@gmail.com>
2025-04-24 21:46:32 +0900

    Delete programing04 directory

--- programing04/OvershadowTest.java
+++
@@ -1,9 +0,0 @@
-package programing04;
-
-public class OvershadowTest {
-	public static void main(String[] args) {
-		Parent p = new Child();
-		System.out.println(p.name);
-		p.print();
-	}
-}
--- programing04/Parent.java
+++
@@ -1,9 +0,0 @@
-package programing04;
-
-public class Parent {
-	String name = "영조";
-			
-		void print() {
-			System.out.println("나는" + name + "이다");
-		}
-}
