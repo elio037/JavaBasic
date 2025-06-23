d4f3ffff3f89ad5fd925d3d3ef204cda2eb2e86c
elio037 <casey030703@gmail.com>
2025-06-23 15:11:21 +0900

    Add files via upload

---
+++ BookMart/src/cart/CartDAO.java
@@ -0,0 +1,11 @@
+package cart;
+
+import java.util.List;
+
+public interface CartDAO {
+	boolean insertCartItem(CartItemVO item);
+	CartItemVO selectCartItem(int bookNo);
+	List<CartItemVO> selectAllCartItem();
+	boolean deleteCartItem(int bookNo);
+	boolean clear();
+}
